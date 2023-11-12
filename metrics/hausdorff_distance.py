a45948305991c53c2a856e15e676e34b84ea585a
BlueDruddigon <haibuihoang1999@gmail.com>
2023-11-12 22:08:14 +0700

    Refactor: Code Refactor

--- metrics/hausdorff_distance.py
+++ metrics/hausdorff_distance.py
@@ -7,4 +7,4 @@ __all__ = ['hd95']
 def hd95(y_true: np.ndarray, y_pred: np.ndarray) -> float:
     y_true[y_true > 0] = 1
     y_pred[y_pred > 0] = 1
-    return metric.binary.hd95(y_true, y_pred) if y_true.sum() > 0 and y_pred.sum() > 0 else 0
+    return metric.binary.hd(y_true, y_pred) if y_true.sum() > 0 and y_pred.sum() > 0 else 0
