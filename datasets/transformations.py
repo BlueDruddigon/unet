68557d7bd4f27666e0ecbd795fca9be40d34d4f7
BlueDruddigon <haibuihoang1999@gmail.com>
2023-11-15 23:14:04 +0700

    Fix: Do not normalize!

--- datasets/transformations.py
+++ datasets/transformations.py
@@ -7,19 +7,16 @@ def get_default_transformations(img_size):
     return {
       'train': T.Compose([
           T.Resize([img_size, img_size], antialias=True),
-          T.Normalize(mean=[0.5], std=[0.5]),
           T.RandomChoice([
             T.RandomHorizontalFlip(),
             T.RandomVerticalFlip(),
           ]),
           T.RandomRotation((-0.2, 0.2)),
-        ]),
+      ]),
       'valid': T.Compose([
           T.Resize([img_size, img_size], antialias=True),
-          T.Normalize(mean=[0.5], std=[0.5]),
-        ]),
+      ]),
       'test': T.Compose([
           T.Resize([img_size, img_size], antialias=True),
-          T.Normalize(mean=[0.5], std=[0.5]),
-        ])
+      ])
     }
